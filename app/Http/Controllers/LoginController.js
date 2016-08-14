44cf49f81dcb084c7bdfa4f73801a3c31ad61e47
Danny Evins <dcastkid@gmail.com>
2016-08-14 13:19:13 -0500

    add login & post logic, create cutsom login fail page

--- app/Http/Controllers/LoginController.js
+++ app/Http/Controllers/LoginController.js
@@ -2,8 +2,36 @@
 
 
 class LoginController {
+
   * create(req, res) {
     yield res.sendView('login.create');
+  }
+
+  * store(req, res) {
+    const { email, password } = req.all();
+
+    try {
+      yield req.auth.attempt(email, password);
+
+      yield req.with({ success: 'Login successful' })
+      .flash();
+
+      res.redirect('/post');
+    } catch (e) {
+      yield req.withAll()
+      .andWith({ error: 'Email and password do not match' })
+      .flash();
+
+      res.redirect('back');
+    }
+  }
+
+  * destroy(req, res) {
+    yield req
+    .with({ success: 'logged out' })
+    .flash();
+
+    res.redirect('/login');
   }
 
 }
--- app/Http/Controllers/PostController.js
+++ app/Http/Controllers/PostController.js
@@ -1,7 +1,26 @@
-'use strict'
+'use strict';
+const Post = use('App/Model/Post');
 
 class PostController {
+  * index(req, res) {
+    const posts = yield Post.with('user').fetch();
 
+    yield res.sendView('post.index', { posts: posts.toJSON() });
+  }
+
+  * create(req, res) {
+    yield res.sendView('post.create');
+  }
+
+  * store(req, res) {
+    const { title, body } = req.all();
+
+    yield req.authUser.posts().create({ title, body });
+    yield req.with({ success: 'Post created successfully' })
+    .flash();
+
+    res.redirect('/post');
+  }
 }
 
-module.exports = PostController
+module.exports = PostController;
